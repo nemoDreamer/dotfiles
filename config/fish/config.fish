84496b575714e5ca2a3cabeb623e7f958d068ed2
Philip Blyth <philip.blyth@gmail.com>
2014-10-07 09:28:55 -0400

    Improved fish config for custom setup

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -11,17 +11,11 @@ set fish_theme bobthefish
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins brew
-set fish_plugins bundler
-set fish_plugins git
-set fish_plugins rails
-set fish_plugins rake
-set fish_plugins rbenv
-set fish_plugins sublime
+set fish_plugins bak brew bundler emoji-clock extract gi jump localhost rbenv sublime
 
 # Custom
 # Path to your custom folder (default path is $FISH/custom)
-#set fish_custom $HOME/.dotfiles/oh-my-fish
+set fish_custom $HOME/.config/fish
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
