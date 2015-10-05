e1c381b607b2a0339a713acda8cf96864e1298af
Philip Blyth <philip.blyth@gmail.com>
2015-10-05 12:18:33 -0400

    Using `z` from tacklebox

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,14 +8,15 @@ set tacklebox_theme bobthefish
 # Which modules would you like to load? (modules can be found in ~/.tackle/modules/*)
 # Custom modules may be added to ~/.tacklebox/modules/
 # Example format: set tacklebox_modules virtualfish virtualhooks
+set tacklebox_modules virtualfish virtualhooks z
 
 # Which plugins would you like to enable? (plugins can be found in ~/.tackle/plugins/*)
 # Custom plugins may be added to ~/.tacklebox/plugins/
 # Example format: set tacklebox_plugins python extract
-set tacklebox_plugins bak balias brew bundler emoji-clock extract extract gi git-flow grc jump localhost node pip python rails sublime up
+set tacklebox_plugins bak balias brew bundler emoji-clock extract gi git-flow grc jump localhost node pip python rails sublime up
 
 # Load Tacklebox configuration
-. ~/.tacklebox/tacklebox.fish
+source ~/.tacklebox/tacklebox.fish
 
 # ---
 
